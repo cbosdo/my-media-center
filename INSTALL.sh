4381046ceadc6554ebb1b03bff98657fb5f588e3
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-01-05 14:32:52 +0100

    Update to python3 and drop the vlc hack

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # install buttonsd
-sudo apt-get install -y build-essential python-dev python-pip python-setuptools netcat
-sudo pip install RPi.GPIO
-sudo ln -s /home/osmc/buttonsd/buttonsd.service /etc/systemd/system/multi-user.target.wants/buttonsd.service
+sudo apt-get install -y build-essential python3-dev python3-pip python3-setuptools netcat
+sudo pip3 install RPi.GPIO
+sudo cp ~osmc/buttonsd/buttonsd.service /usr/lib/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start buttonsd
+sudo systemctl enable --now buttonsd
 
 # install vlc player + sound settings
 sudo apt-get install -y alsa-utils
@@ -13,8 +13,12 @@ sudo amixer cset numid=3 1
 sudo ln -s /home/osmc/cdrom.rules /etc/udev/rules.d/82-cdrom.rules
 cp kodi-files/userdata/playercorefactory.xml ~/.kodi/userdata
 
+# install libdvdcss
+sudo apt-get install -y libdvd-pkg
+sudo dpkg-reconfigure libdvd-pkg
+
 # enable i2c
-sudo apt-get install -y python-smbus i2c-tools
+sudo apt-get install -y python3-smbus i2c-tools
 echo '>>> Enable I2C'
 if grep -q 'i2c-bcm2708' /etc/modules; then
   echo 'Seems i2c-bcm2708 module already exists, skip this step.'
@@ -44,13 +48,16 @@ else
 fi
 
 # install OLED
-sudo apt-get install -y lcddproc
+sudo apt-get install -y lcdproc
 git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
 cd Adafruit_Python_SSD1306
-sudo python setup.py install
+sudo python3 setup.py install
 sudo ln -s /home/osmc/lcdd-oled/lcdd-oled.service /etc/systemd/system/multi-user.target.wants/lcdd-oled.service
 kodi-send --action="InstallAddon(script.xbmc.lcdproc)"
 cp kodi-files/userdata/LCD.xml ~/.kodi/userdata
 
 
 # TODO Install Replay Add-ons
+kodi-send --action="InstallAddon(plugin.video.francetv)"
+kodi-send --action="InstallAddon(plugin.video.catchuptvandmore)"
+kodi-send --action="InstallAddon(plugin.audio.jambmc)"
